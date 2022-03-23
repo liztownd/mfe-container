5060a2c290828b93d8e8a742ce13eabe67b97063
Liz Townsend <elizabeth.townsend@torch.ai>
2022-03-23 14:07:23 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import MarketingAppComponent from './components/MarketingApp';
+
+const App = () => {
+    
+    return (
+    <div>
+        <h1>Hi there!</h1>
+        <hr/>
+    <MarketingAppComponent />
+    </div>)
+}
+
+export default App;
