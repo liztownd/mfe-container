e987a70fa438db970a4907a6de6eccd7f564f40d
Liz Townsend <elizabeth.townsend@torch.ai>
2022-03-24 17:02:51 -0500

    prefix production classNames

--- src/App.js
+++ src/App.js
@@ -2,14 +2,24 @@ import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import MarketingAppComponent from "./components/MarketingApp";
 import Header from "./components/Header";
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from "@material-ui/core/styles";
 
 const App = () => {
+  const generateClassName = createGenerateClassName({
+    productionPrefix: "co",
+  });
+
   return (
     <BrowserRouter>
-      <div>
-        <Header />
-        <MarketingAppComponent />
-      </div>
+      <StylesProvider generateClassName={generateClassName}>
+        <div>
+          <Header />
+          <MarketingAppComponent />
+        </div>
+      </StylesProvider>
     </BrowserRouter>
   );
 };
