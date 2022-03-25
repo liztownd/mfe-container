53a8246bbe3a7e6a394a547a74a4b81d31b73cce
Liz Townsend <elizabeth.townsend@torch.ai>
2022-03-25 12:56:01 -0500

    add navigation between apps

--- src/components/MarketingApp.js
+++ src/components/MarketingApp.js
@@ -1,16 +1,29 @@
-import { mount } from 'marketing/MarketingApp';
-import React, { useRef, useEffect } from 'react';
+import { mount } from "marketing/MarketingApp";
+import React, { useRef, useEffect } from "react";
+import { useHistory } from "react-router-dom";
 
 const MarketingAppComponent = () => {
-    const ref = useRef(null);
+  const ref = useRef(null);
+  const history = useHistory();
 
-    useEffect(() => {
-        mount(ref.current);
-    }, []);
+  useEffect(() => {
+      const { onParentNavigate } =
+    mount(ref.current, {
+      // keep BrowserHistory in sync
+      onNavigate: ({ pathname: nextPathname }) => {
+        const { pathname } = history.location;
 
-    return (
-        <div ref={ref} />
-    )
-}
+        if (pathname !== nextPathname) {
+          history.push(nextPathname);
+        }
+      },
+    });
+
+    history.listen(onParentNavigate);
+
+  }, []);
+
+  return <div ref={ref} />;
+};
 
 export default MarketingAppComponent;
