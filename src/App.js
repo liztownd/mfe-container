b07f32cca5c8e3f32f0ca35bccbd3590511692ca
Liz Townsend <elizabeth.townsend@torch.ai>
2022-03-30 16:23:53 -0500

    add auth

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { lazy, Suspense } from "react";
+import React, { lazy, Suspense, useState } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Header from "./components/Header";
 import {
@@ -15,16 +15,23 @@ const App = () => {
     productionPrefix: "co",
   });
 
+  const [isSignedIn, setIsSignedIn] = useState(false);
+
   return (
     <BrowserRouter>
       <StylesProvider generateClassName={generateClassName}>
         <div>
-          <Header />
+          <Header
+            isSignedIn={isSignedIn}
+            onSignOut={() => setIsSignedIn(false)}
+          />
           <Suspense fallback={<Progress />}>
-          <Switch>
-            <Route path="/auth" component={AuthAppComponent} />
-            <Route path="/" component={MarketingAppComponent} />
-          </Switch>
+            <Switch>
+              <Route path="/auth">
+                <AuthAppComponent onSignIn={() => setIsSignedIn(true)} />
+              </Route>
+              <Route path="/" component={MarketingAppComponent} />
+            </Switch>
           </Suspense>
         </div>
       </StylesProvider>
--- src/components/AuthApp.js
+++ src/components/AuthApp.js
@@ -2,14 +2,13 @@ import { mount } from "auth/AuthApp";
 import React, { useRef, useEffect } from "react";
 import { useHistory } from "react-router-dom";
 
-const AuthAppComponent = () => {
+const AuthAppComponent = ({ onSignIn }) => {
   const ref = useRef(null);
   const history = useHistory();
 
   useEffect(() => {
-      const { onParentNavigate } =
-    mount(ref.current, {
-        initialPath: history.location.pathname,
+    const { onParentNavigate } = mount(ref.current, {
+      initialPath: history.location.pathname,
       // keep BrowserHistory in sync
       onNavigate: ({ pathname: nextPathname }) => {
         const { pathname } = history.location;
@@ -18,10 +17,10 @@ const AuthAppComponent = () => {
           history.push(nextPathname);
         }
       },
+      onSignIn,
     });
 
     history.listen(onParentNavigate);
-
   }, []);
 
   return <div ref={ref} />;
